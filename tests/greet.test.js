6cadbd5736f0c09550585817f85ffb76f2ba0d89
Tshilidzi-Mamatho <100323518+Tshilidzi-Mamatho@users.noreply.github.com>
2022-08-25 01:05:26 +0200

    edit

--- tests/greet.test.js
+++ tests/greet.test.js
@@ -1,6 +1,8 @@
 describe('greet Function' , function(){
     it('should return Hello and name' , function(){
         assert.equal(greet('Tshilidzi'), 'Hello, Tshilidzi');
+    });
+    it('should return Hello and name' , function(){
         assert.equal(greet('Mamatho'), 'Hello, Mamatho');
     });
 
