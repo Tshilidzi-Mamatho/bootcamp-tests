fffcdbc53d4cd73c89f12e63eb1b76c9d9ded883
Tshilidzi-Mamatho <100323518+Tshilidzi-Mamatho@users.noreply.github.com>
2022-08-25 17:39:14 +0200

    some changes

--- tests/transportFee.test.js
+++ tests/transportFee.test.js
@@ -1,7 +1,11 @@
 describe('transportFee Function' , function(){
-    it('should return R20 for morning, R10 for afternoon and free for nightshift' , function(){
-        assert.equal(transportFee('morning'), 'R20');
-        assert.equal(transportFee('afternoon'), 'R10');
-        assert.equal(transportFee('nightshift'), 'free');
+    it('should return R20 for morning' , function(){
+        assert.equal(transportFee('morning'), 'R20')
+    });
+    it('should return R10 for afternoon' , function(){
+        assert.equal(transportFee('afternoon'), 'R10')
+    });
+    it('should return free for nightshift' , function(){
+        assert.equal(transportFee('nightshift'), 'free')
     });
 });
